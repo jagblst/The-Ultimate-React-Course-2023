cbfed675d6b88ff86352cf7a578fe043ba5ece4d
Jaga <jagblst@gmail.com>
2023-11-01 13:30:02 +0200

    Usepopcorn app
    Passing Elements as Props (Alternative to children)

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -62,6 +62,16 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />{" "}
+            </>
+          }
+        />
+        {/*
         <Box>
           <MovieList movies={movies} />
         </Box>
@@ -69,6 +79,7 @@ export default function App() {
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
         </Box>
+        */}
       </Main>
     </>
   );
@@ -112,7 +123,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -120,7 +131,7 @@ function Box({ children }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
