0b88ce5a9b39267d890f07b01b5716725096cd23
Jaga <jagblst@gmail.com>
2023-11-22 10:52:29 +0200

    WorldWise app
    Setting Map Position With Geolocation

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -5,20 +5,25 @@ import {
   TileLayer,
   Marker,
   Popup,
-  useMapEvent,
   useMapEvents,
 } from "react-leaflet";
 import { useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useMap } from "react-leaflet";
 import { useEffect } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -29,8 +34,21 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
